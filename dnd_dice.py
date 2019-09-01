fa62b78bbd8dff9557a220677ce1ee02827d9922
joe-wyze <49135765+joe-wyze@users.noreply.github.com>
2019-09-01 01:15:51 -0400

    Add files via upload
    Dice selection now done with a dictionary. Code runs faster when using fewer dice.

--- dnd_dice.py
+++ dnd_dice.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+DnD dice
+"""
+
 
 import random
 
 #You can also set them equal to 1 or 0
 usedict = {
     'd20':True,
-    'd100':False,
-    'd12':False,
-    'd10':False,
-    'd8':False,
+    'd100':True,
+    'd12':True,
+    'd10':True,
+    'd8':True,
     'd6a':True,
-    'd6b':False,
-    'd4':False,
+    'd6b':True,
+    'd4':True,
     }
 
 rolldict = {
@@ -24,10 +29,19 @@ rolldict = {
     'd4':0,
     }
 
+setdict = {
+        'd20':(1,20,1),
+        'd100':(10,100,10),
+        'd12':(1,12,1),
+        'd10':(1,10,1),
+        'd8':(1,8,1),
+        'd6a':(1,6,1),
+        'd6b':(1,6,1),
+        'd4':(1,4,1),
+        }
 
-#dicelist = [d20,d100,d12,d10,d8,d6a,d6b,d4]
-#dicerolllist = [d20roll,d100roll,d12roll,d10roll,d8roll,d6aroll,d6broll,d4roll]
 
+'''
 rolldict['d20'] = random.randrange(1,20,1)*int(usedict['d20'])
 rolldict['d100'] = random.randrange(10,100,10)*int(usedict['d100'])
 rolldict['d12'] = random.randrange(1,12,1)*int(usedict['d12'])
@@ -36,6 +50,7 @@ rolldict['d8'] = random.randrange(1,8,1)*int(usedict['d8'])
 rolldict['d6a'] = random.randrange(1,6,1)*int(usedict['d6a'])
 rolldict['d6b'] = random.randrange(1,6,1)*int(usedict['d6b'])
 rolldict['d4'] = random.randrange(1,4,1)*int(usedict['d4'])
+'''
 
 #totalroll = d20roll+d100roll+d12roll+d10roll+d8roll+d6aroll+d6broll+d4roll
 
@@ -43,10 +58,8 @@ totalroll = 0
 
 for d in rolldict.keys():
     if bool(usedict[d])==True:
+        rolldict[d] = random.randrange(setdict[d][0],setdict[d][1],setdict[d][2])
         print(d + ' rolls '+ str(rolldict[d]))
-    totalroll = totalroll + rolldict[d]
+        totalroll = totalroll + rolldict[d]
 
 print('Total = ' + str(totalroll))
-
-
-
